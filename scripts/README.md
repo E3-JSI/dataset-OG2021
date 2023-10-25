874e11b4a013864eca8f6148111f39ed120de79b
Erik Calcina <erik.calcina@gmail.com>
2023-10-25 14:18:25 +0200

    Updating scripts, README file with instructions

--- scripts/README.md
+++ scripts/README.md
@@ -13,3 +13,43 @@ This folder contains the scripts for:
    ```bash
    bash run_news_collector.sh
    ```
+
+
+Here we explain how to recreate the experiment with our code.
+
+
+1. Divide the dataset into concepts created by Event Registry using the following python script:
+
+```
+create_datasets.py
+```
+This script will create a new folder in data named concepts (data/concepts). When finished the folder will contain csv files which names represent the containing concept.
+
+2. After that use:
+
+``` 
+PairBERT_labeling.py
+```
+This script need two parameters the input csv file (one of csv files created in the first step) and the output (where do you want the csv file to be saved).
+
+3. Split dataset by concepts
+
+```
+02_split_by_concepts.py
+```
+Script partitions the entire dataset into distinct concepts (column concepts), creating a separate CSV file for each concept.
+
+4. Now we can start with labeling
+
+```
+03_PairBERT_concepts_labeling.py
+```
+This script employs script named PairBERT_labeling to separately cluster articles within each concept into event-based clusters. Input of this script is the file (data/concepts) created by the script in the first step.
+
+5. Merging labeled concepts in one file.
+
+```
+04_concepts_merging.py
+```
+This script merges together concepts into a single CSV file. Input of this script is the file where labečed concepts were saved by script ```03_PairBERT_concepts_labeling.py```.
+
---
+++ src/utils/PairBERT2.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import lightning.pytorch as pl
+from transformers import AutoModel, AutoTokenizer, logging
+import torchmetrics
+
+from typing import List
+
+# set the verbosity warning
+logging.set_verbosity_error()
+
+
+# =====================================
+# Helper Functions
+# =====================================
+
+
+def mean_pooling(model_output: torch.Tensor, attention_mask: torch.Tensor):
+    """Mean pooling - considers attention mask for correct averaging
+
+    Args:
+        model_output (Tensor): The model's output containing the token vectors.
+        attention_mask (Tensor): The attention mask showing which tokens to use.
+
+    Returns:
+        Tensor: The tensors containing the mean pooled token embeddings.
+    """
+
+    # First element of model_output contains all token embeddings
+    token_embeds = model_output[0]
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeds.size()).float()
+    )
+    return torch.sum(token_embeds * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
+
+
+# =====================================
+# Main Pairing Model
+# =====================================
+
+
+class PairBERT(pl.LightningModule):
+    def __init__(
+        self,
+        model_id: str,
+        eps: float = 1e-5,
+        lr: float = 1e-5,
+        wd: float = 1e-2,
+        eval_th: float = 0.5,
+    ):
+        super().__init__()
+
+        # save the model hyperparameters
+        self.save_hyperparameters("model_id", "eps", "lr", "wd", "eval_th")
+
+        # get language model sprecific values
+        self.lm = AutoModel.from_pretrained(model_id)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.cos = torch.nn.CosineSimilarity(dim=1, eps=1e-8)
+
+        # add evaluation metrics
+        self.eval_accuracy = torchmetrics.Accuracy(task="binary", threshold=eval_th)
+        self.eval_precision = torchmetrics.Precision(task="binary", threshold=eval_th)
+        self.eval_recall = torchmetrics.Recall(task="binary", threshold=eval_th)
+        self.eval_f1 = torchmetrics.F1Score(task="binary", threshold=eval_th)
+
+    def forward(self, input1: List[str], input2: List[str]):
+        """Classifies if the two text should be paired or not"""
+
+        assert len(input1) == len(
+            input2
+        ), "The length of input1 is not the same as the length of input2"
+
+        # get the inputs' encodings
+        encoded_input1 = self.tokenizer(
+            input1, padding=True, truncation=True, return_tensors="pt"
+        )
+        encoded_input2 = self.tokenizer(
+            input2, padding=True, truncation=True, return_tensors="pt"
+        )
+        # get the inputs' embeddings
+        embedding_input1 = self._get_embeddings(encoded_input1)
+        embedding_input2 = self._get_embeddings(encoded_input2)
+
+        # scale the cosine values from [-1, 1] to [0, 1]
+        logits = (1 + self.cos(embedding_input1, embedding_input2)) / 2
+        return logits
+
+    def configure_optimizers(self):
+        optimizer = optim.AdamW(
+            self.parameters(),
+            lr=self.hparams.lr,
+            weight_decay=self.hparams.wd,
+            eps=self.hparams.eps,
+        )
+        return optimizer
+
+    def on_training_start(self):
+        self.logger.log_hyperparams(self.hparams)
+
+    def training_step(self, train_batch, batch_idx):
+        loss = self._shared_eval_step(train_batch, batch_idx, stage="train")
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        loss = self._shared_eval_step(val_batch, batch_idx, stage="val")
+        self.log("val_loss", loss, on_epoch=True)
+        return loss
+
+    def on_validation_epoch_end(self):
+        self._shared_log("val_metrics")
+
+    def test_step(self, test_batch, batch_idx):
+        loss = self._shared_eval_step(test_batch, batch_idx, stage="test")
+        self.log("test_loss", loss, on_epoch=True)
+        return loss
+
+    def on_test_epoch_end(self):
+        self._shared_log("test_metrics")
+
+    def _shared_eval_step(self, batch, batch_idx, stage):
+        # get the input encodings
+        encoded_input1 = batch["encoded_input1"]
+        encoded_input2 = batch["encoded_input2"]
+        target = batch["labels"]
+
+        # get the inputs' embeddings
+        embedding_input1 = self._get_embeddings(encoded_input1)
+        embedding_input2 = self._get_embeddings(encoded_input2)
+
+        # scale the values from [-1, 1] to [0, 1]
+        logits = (1 + self.cos(embedding_input1, embedding_input2)) / 2
+
+        # get the loss of the model
+        loss = self._get_loss(logits, target)
+
+        if stage in ["val", "test"]:
+            self.eval_accuracy(logits, target)
+            self.eval_precision(logits, target)
+            self.eval_recall(logits, target)
+            self.eval_f1(logits, target)
+
+        return loss
+
+    def _get_embeddings(self, encoded_input):
+        """Calculates the sentence embeddings of the texts
+
+        Args:
+            encoded_input: The dictionary containing the encoding tensors.
+
+        Returns:
+            Tensor: The tensor containing the token embeddings.
+
+        """
+        model_output = self.lm(**encoded_input)
+        embeddings = mean_pooling(model_output, encoded_input["attention_mask"])
+        embeddings = F.normalize(embeddings, p=2, dim=1)
+        return embeddings
+
+    def _shared_log(self, state):
+        self.log_dict(
+            {
+                f"{state}_accuracy": self.eval_accuracy,
+                f"{state}_precision": self.eval_precision,
+                f"{state}_recall": self.eval_recall,
+                f"{state}_f1score": self.eval_f1,
+            }
+        )
+
+    def _get_loss(self, logits, target):
+        criterion = nn.BCEWithLogitsLoss()
+        return criterion(logits, target.to(torch.float32))
